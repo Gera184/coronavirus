ffe1a2fd440cd5f800cdc6ba239a6a60adb78563
Gera184 <oogeraoo159@gmail.com>
2021-03-16 14:15:27 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Main } from "./components/main/Main";
+import { Header } from "./components/header/Header";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import CoronaMap from "./components/map/CoronaMap";
 
-function App() {
+export default () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Router>
+        <Switch>
+          <Route exact path="/home" component={Main} />
+          <Route exact path="/" component={Main} />
+          <Route exact path="/map" component={CoronaMap} />\
+        </Switch>
+      </Router>
     </div>
   );
-}
-
-export default App;
+};
---
+++ src/components/map/CoronaMap.js
@@ -0,0 +1,117 @@
+import React, { useState, useEffect } from "react";
+import {
+  GoogleMap,
+  withScriptjs,
+  withGoogleMap,
+  Marker,
+  InfoWindow,
+} from "react-google-maps";
+
+import "./CoronaMap.css";
+import axios from "axios";
+import { Container, Row, Col } from "react-bootstrap";
+import uuid from "react-uuid";
+
+
+
+const options = {
+  method: "GET",
+  url: "https://covid-19-statistics.p.rapidapi.com/reports",
+  headers: {
+    "x-rapidapi-key": "9da9f853dcmsh86bba916aa656c2p18fc8djsne2562db22529",
+    "x-rapidapi-host": "covid-19-statistics.p.rapidapi.com",
+  },
+};
+
+function Map() {
+  const [selected, setSelected] = useState(null);
+  const [queries, setQueries] = useState([]);
+
+  useEffect(() => {
+    axios
+      .request(options)
+      .then((res) => {
+        console.log(res.data.data);
+        setQueries(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <GoogleMap defaultZoom={8} defaultCenter={{ lat: 32.06616, lng: 34.79485 }}>
+      {queries.map((query) => (
+        <Marker
+          key={uuid()}
+          position={{
+            lat: parseInt(query.region.lat, 10),
+            lng: parseInt(query.region.long, 10),
+          }}
+          onClick={() => {
+            setSelected(query);
+          }}
+          icon={{
+            url:
+              "https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/SARS-CoV-2_without_background.png/220px-SARS-CoV-2_without_background.png",
+            scaledSize: new window.google.maps.Size(30, 30),
+            origion: new window.google.maps.Point(0, 0),
+            anchor: new window.google.maps.Point(15, 15),
+          }}
+        />
+      ))}
+
+      {selected && (
+        <InfoWindow
+          position={{
+            lat: parseInt(selected.region.lat, 10),
+            lng: parseInt(selected.region.long, 10),
+          }}
+          onCloseClick={() => {
+            setSelected(null);
+          }}
+        >
+          <Container className="info-window-con" fluid>
+            <Row>
+              <Col>
+                <span className="info-details">
+                  Country: {selected.region.name}{" "}
+                </span>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <span className="info-details">
+                  Active: {selected.active} ,{" "}
+                </span>
+                <span className="info-deaths">Deaths: {selected.deaths} </span>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <span className="info-recovered">
+                  Recovered: {selected.recovered}{" "}
+                </span>
+              </Col>
+            </Row>
+          </Container>
+        </InfoWindow>
+      )}
+    </GoogleMap>
+  );
+}
+
+const WrappedMap = withScriptjs(withGoogleMap(Map));
+
+export default function CoronaMap() {
+  return (
+    <div>
+      <WrappedMap
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCirpZxwCE6TZ74bxOHXcXFianoChEmfTg`}
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `500px` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+    </div>
+  );
+}
